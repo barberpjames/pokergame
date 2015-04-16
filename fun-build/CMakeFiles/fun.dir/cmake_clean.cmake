1893709161481711702f52655342428da8fb9c4f
barberpjames <jpb58@zips.uakron.edu>
2015-04-16 10:00:08 -0400

    Adding more files

---
+++ fun-build/CMakeFiles/fun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fun.dir/fun.cpp.o"
+  "fun.pdb"
+  "fun"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
